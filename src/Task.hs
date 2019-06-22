c36ba2ccc582acd504c0d1baefdfef828898fb53
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-22 14:44:47 +0900

    apply L8 namespace.

--- src/Task.hs
+++ src/Task.hs
@@ -14,7 +14,6 @@ import Data.List
 import Data.Attoparsec.ByteString.Lazy
   (Parser, parse, eitherResult)
 import Data.Attoparsec.ByteString.Char8 hiding (parse, eitherResult)
-import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString.Lazy.Char8 as L8
 
 
@@ -24,10 +23,10 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 -----------------------------------------------------
 -- 3.2 Encoding solutions
 
-type PrintM = Writer (DList LB.ByteString)
+type PrintM = Writer (DList L8.ByteString)
 type Printer a = a -> PrintM ()
 
-runPrinter :: Printer a -> a -> LB.ByteString
+runPrinter :: Printer a -> a -> L8.ByteString
 runPrinter p = mconcat . DList.toList . execWriter . p
 
 data Action
@@ -46,7 +45,7 @@ data Action
   | ActionC
   deriving (Eq, Ord, Show)
 
-encodeAction :: Action -> LB.ByteString
+encodeAction :: Action -> L8.ByteString
 encodeAction = enc
   where
     enc ActionW = "W"
@@ -101,7 +100,7 @@ type Solution = [Actions]
 printSolution :: Printer Solution
 printSolution = sequence_ . intersperse (tell (pure "#")) . map printActions
 
-parseSolution :: LB.ByteString -> Either String Solution
+parseSolution :: L8.ByteString -> Either String Solution
 parseSolution = runParser (solutionP <* endOfInput)
 
 solutionP :: Parser Solution
@@ -136,7 +135,7 @@ data BoosterCode
 -- >>> parseTask (L8.pack "(15,23),(16,23),(16,17),(15,17),(15,20),(12,20),(12,19),(10,19),(10,16),(12,16),(12,17),(13,17),(13,14),(14,14),(14,8),(16,8),(16,15),(18,15),(18,0),(27,0),(27,15),(22,15),(22,23),(19,23),(19,25),(24,25),(24,27),(18,27),(18,33),(16,33),(16,32),(11,32),(11,30),(16,30),(16,27),(15,27),(15,25),(13,25),(13,24),(12,24),(12,29),(9,29),(9,27),(8,27),(8,37),(2,37),(2,27),(3,27),(3,24),(9,24),(9,23),(0,23),(0,22),(9,22),(9,21),(13,21),(13,22),(15,22)#(0,22)#(20,7),(24,7),(24,5),(22,5),(22,6),(21,6),(21,5),(19,5),(19,4),(20,4),(20,3),(19,3),(19,2),(20,2),(20,1),(21,1),(21,4),(22,4),(22,3),(23,3),(23,4),(24,4),(24,3),(25,3),(25,7),(26,7),(26,13),(24,13),(24,14),(23,14),(23,13),(22,13),(22,14),(21,14),(21,13),(20,13)#X(16,25);L(19,19);F(4,30);F(17,21);B(4,31)")
 -- Right (Task {taskMap = [(15,23),(16,23),(16,17),(15,17),(15,20),(12,20),(12,19),(10,19),(10,16),(12,16),(12,17),(13,17),(13,14),(14,14),(14,8),(16,8),(16,15),(18,15),(18,0),(27,0),(27,15),(22,15),(22,23),(19,23),(19,25),(24,25),(24,27),(18,27),(18,33),(16,33),(16,32),(11,32),(11,30),(16,30),(16,27),(15,27),(15,25),(13,25),(13,24),(12,24),(12,29),(9,29),(9,27),(8,27),(8,37),(2,37),(2,27),(3,27),(3,24),(9,24),(9,23),(0,23),(0,22),(9,22),(9,21),(13,21),(13,22),(15,22)], taskPoint = (0,22), taskObstacles = [[(20,7),(24,7),(24,5),(22,5),(22,6),(21,6),(21,5),(19,5),(19,4),(20,4),(20,3),(19,3),(19,2),(20,2),(20,1),(21,1),(21,4),(22,4),(22,3),(23,3),(23,4),(24,4),(24,3),(25,3),(25,7),(26,7),(26,13),(24,13),(24,14),(23,14),(23,13),(22,13),(22,14),(21,14),(21,13),(20,13)]], taskBoosters = [(BoosterX,(16,25)),(BoosterL,(19,19)),(BoosterF,(4,30)),(BoosterF,(17,21)),(BoosterB,(4,31))]})
 
-parseTask :: LB.ByteString -> Either String Task
+parseTask :: L8.ByteString -> Either String Task
 parseTask = runParser (taskP <* endOfInput)
 
 
@@ -191,5 +190,5 @@ boosterCodeP = do
 
 -----
 
-runParser :: Parser a -> LB.ByteString -> Either String a
+runParser :: Parser a -> L8.ByteString -> Either String a
 runParser p = eitherResult . parse p
