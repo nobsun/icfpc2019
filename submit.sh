fa073d4403454e085c690f78fc20970362417e82
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-22 18:33:46 +0900

    update submit front-end.

--- submit.sh
+++ submit.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 
+set -e
+
+usage() {
+    cat <<EOF
+Usage: $0 DIRECTORY_TO_SUBMIT
+EOF
+}
+
+dir="$1"
+
+if [ x"$dir" = x ]; then
+    echo "DIRECTORY_TO_SUBMIT is required."
+    usage
+    exit 1
+fi
+
+if [ ! -d "$dir" ]; then
+    echo "directory not found: $dir"
+    usage
+    exit 1
+fi
+
+cwd=$(pwd)
 ts=$(date +"%d-%H%M%S-%N")
 
-set -e
 set -x
 
-rm -f solutions.zip
-rm -f solutions/*~
-( cd solutions/ && zip -r ../solutions.zip . )
+rm -f ${cwd}/solutions.zip
 
-unzip -l solutions.zip
+( cd $dir && zip -r ${cwd}/solutions.zip *.sol )
 
-cp -a solutions.zip /home/icfpc2019/submit/post/${ts}.zip
+# unzip -l ${cwd}/solutions.zip
+
+cp -a ${cwd}/solutions.zip /home/icfpc2019/submit/post/${ts}.zip
