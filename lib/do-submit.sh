db40b70c0e3f00ae7705d418a886488db96c33f7
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-22 17:18:50 +0900

    add internal submit script.

---
+++ lib/do-submit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+zip_path="$1"
+
+if [ ! -r $zip_path ]; then
+    cat <<EOF
+file not found: $zip_path
+
+Usage: $0 ZIPFILE_PATH
+EOF
+fi
+
+log_path=$(dirname $zip_path)/$(basename $zip_path .zip).log
+
+pvfile=/home/icfpc2019/data/private-id.txt
+
+set -e
+
+curl \
+    -F private_id="$(cat $pvfile)" \
+    -F file="@${zip_path}" \
+    https://monadic-lab.org/submit \
+    >> $log_path 2>&1
+
+cat $log_path
+echo ''
