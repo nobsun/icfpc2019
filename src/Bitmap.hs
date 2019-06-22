ce9e35e315e9b0bef0f7c42e85d171011f1dd028
Masahiro Sakai <masahiro.sakai@gmail.com>
2019-06-22 23:28:35 +0900

    add isVisible to Bitmap.hs

--- src/Bitmap.hs
+++ src/Bitmap.hs
@@ -23,3 +23,26 @@ bitmapToLines bm = do
 
 printBitmap :: UArray Point Bool -> IO ()
 printBitmap = mapM_ putStrLn . bitmapToLines
+
+isVisible :: UArray Point Bool -> Point -> Point -> Bool
+isVisible bm from to = all (bm !) $ passingCells from to
+
+passingCells :: Point -> Point -> [Point]
+passingCells p0@(x0,y0) p1@(x1,y1)
+  | x0 == x1 = [(x0,y) | y <- [min y0 y1 .. max y0 y1]]
+  | y0 == y1 = [(x,y0) | x <- [min x0 x1 .. max x0 x1]]
+  | otherwise = concat $
+      [ if max l r <= toRational (floor (min l r) + 1)
+        then [(x, floor (min l r))]
+        else [(x, floor (min l r)), (x, floor (min l r) + 1)]
+      | x <- [x0, x0 + signum (x1 - x0) .. x1]
+      , let l = f (fromIntegral x)
+      , let r = f (fromIntegral x + 1)
+      ]
+      where
+        a :: Rational
+        a = fromIntegral (y1 - y0) / fromIntegral (x1 - x0)
+
+        -- (x0 + 0.5, y0 + 0.5) を通る傾き a の直線
+        f :: Rational -> Rational
+        f x = a * (x - (fromIntegral x0 + 0.5)) + (fromIntegral y0 + 0.5)
