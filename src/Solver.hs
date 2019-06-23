be0c93ce468a554b591552590af55924a3731bd5
cutsea110 <cutsea110@gmail.com>
2019-06-23 10:59:17 +0900

    implement readProb

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,12 +1,12 @@
 module Solver where
 
-import Data.ByteString.Lazy
+import Data.ByteString.Lazy as BL
 
 import Task
 import WorkerWrapper
 
+readProb :: FilePath -> IO State
+readProb path = return . initState =<< BL.readFile path
+  
 initState :: ByteString -> State
-initState prob = s
-  where
-    Right t = parseTask prob
-    s = initialState t
+initState = either error initialState . parseTask . BL.init
