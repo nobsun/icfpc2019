d4d89fcdfe6262991762fab02d5ea17268b61cae
cutsea110 <cutsea110@gmail.com>
2019-06-24 01:29:28 +0900

    catch up script for part-3

--- solve-by-simple-solver.sh
+++ solve-by-simple-solver.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 ls part-1-initial/*.desc | sed 's/part-1-initial\/\(.*\)\.desc/.\/solver part-1-initial\/\1.desc --alg=simple > solutions-simple-solver\/\1.sol \&/'|sh
+
 ls part-2-teleports/*.desc | sed 's/part-2-teleports\/\(.*\)\.desc/.\/solver part-2-teleports\/\1.desc --alg=simple > solutions-simple-solver\/\1.sol \&/'|sh
+
+ls part-3-clones/*.desc | sed 's/part-3-clones\/\(.*\)\.desc/.\/solver part-3-clones\/\1.desc --alg=simple > solutions-simple-solver\/\1.sol \&/'|sh
 
--- solve-by-solver-simpleprime.sh
+++ solve-by-solver-simpleprime.sh
@@ -3,3 +3,5 @@
 ls part-1-initial/*.desc | sed 's/part-1-initial\/\(.*\)\.desc/.\/solver part-1-initial\/\1.desc --alg=simple-prime > solutions-solver-simpleprime\/\1.sol \&/'|sh
 
 ls part-2-teleports/*.desc | sed 's/part-2-teleports\/\(.*\)\.desc/.\/solver part-2-teleports\/\1.desc --alg=simple-prime > solutions-solver-simpleprime\/\1.sol \&/'|sh
+
+ls part-3-clones/*.desc | sed 's/part-3-clones\/\(.*\)\.desc/.\/solver part-3-clones\/\1.desc --alg=simple-prime > solutions-solver-simpleprime\/\1.sol \&/'|sh
