85c5134a8fe5596cc2983ceb8a2d6c9001ab53dc
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-24 17:21:17 +0900

    add solveConcurrent.

--- src/RunConcurrent.hs
+++ src/RunConcurrent.hs
@@ -1,10 +1,12 @@
 module RunConcurrent  (
-  newTaskQueue, enqueueTask, runConcurrent,
+  solveConcurrent,
   ) where
 
 import Control.Monad
 import Control.Concurrent (forkIO, ThreadId)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
+
+import RunSolver (Algo, callSolver)
 
 type TaskQueue a = Chan (Maybe a)
 
@@ -13,6 +15,9 @@ newTaskQueue = newChan
 
 enqueueTask :: TaskQueue a -> a -> IO ()
 enqueueTask q = writeChan q . Just
+
+finalizeTaskQueue :: Int -> TaskQueue a -> IO ()
+finalizeTaskQueue n q = replicateM_ n (writeChan q Nothing)
 
 runConcurrent :: Int
               -> TaskQueue a
@@ -24,3 +29,14 @@ runConcurrent n q action =
     loop = do
       mt <- readChan q
       maybe (return ()) (\t -> action t >> loop) mt
+
+-----
+
+solveConcurrent :: Int
+                -> [(Int, Algo)]
+                -> IO ()
+solveConcurrent para probs = do
+  tq <- newTaskQueue
+  void $ runConcurrent para tq (uncurry callSolver)
+  mapM_ (enqueueTask tq) probs
+  finalizeTaskQueue para tq
