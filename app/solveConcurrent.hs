f0b437fd2289233a0102ca7bc9ccfa092fd00efb
Kei Hibino <ex8k.hibino@gmail.com>
2019-06-24 17:54:06 +0900

    update argument.

--- app/solveConcurrent.hs
+++ app/solveConcurrent.hs
@@ -1,3 +1,4 @@
+import RunSolver (Algo (..))
 import RunConcurrent (solveConcurrent)
 
 import System.Environment
@@ -5,8 +6,11 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  (n, x, algo) <- case args of
-    n_ : x_ : algo_ : _ -> (,,) <$> readIO n_ <*> readIO x_ <*> readIO algo_
+  (n, x, malgo) <- case args of
+    n_ : x_ : algo_ : _ -> (,,) <$> readIO n_ <*> readIO x_ <*> fmap Just (readIO algo_)
+    n_ : x_ : _         -> (,,) <$> readIO n_ <*> readIO x_ <*> pure Nothing
     _                   -> fail "invalid argument: solve-concurrent MIN MAX ALGORITHM_NAME"
 
-  solveConcurrent 38 [ (i, algo) | i <- [n..x] ]
+  case malgo of
+    Nothing   -> solveConcurrent 38 [ (i, algo) | i <- [n..x], algo <- [ SimplePrime ] ]
+    Just algo -> solveConcurrent 38 [ (i, algo) | i <- [n..x] ]
